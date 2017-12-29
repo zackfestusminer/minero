4f9e7be223e464fdebd20fcb3ea811349d79f8fe
anon <anon@anon.anon>
2017-12-29 08:52:09 -0500

    init

---
+++ StartStop/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace StartStop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblErrorCount = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(129, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblErrorCount
+            // 
+            this.lblErrorCount.AutoSize = true;
+            this.lblErrorCount.Location = new System.Drawing.Point(25, 19);
+            this.lblErrorCount.Name = "lblErrorCount";
+            this.lblErrorCount.Size = new System.Drawing.Size(35, 13);
+            this.lblErrorCount.TabIndex = 6;
+            this.lblErrorCount.Text = "label1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(242, 49);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 41);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(16, 118);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(92, 44);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 238);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.lblErrorCount);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblErrorCount;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ StartStop/Form1.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Configuration;
+using System.Management;
+
+namespace StartStop
+{
+
+
+
+    public partial class Form1 : Form
+    {
+        bool run = true;
+        int errCount = 0;
+        string _processName = string.Empty;
+        int _runFor = 20;
+        int _pauseFor = 1;
+
+        //  0 = no just 1
+        //  1 = alternate 1 and 2
+        //  2 = 2 always
+        bool _twoNonVegaThreads = false;
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private string AppSetting(string setting)
+        {
+
+            return ConfigurationManager.AppSettings[setting].ToString();
+        }
+
+
+        private void InitVariables()
+        {
+            _runFor = Convert.ToInt32(ConfigurationManager.AppSettings["runFor"]) * 1000;
+            _pauseFor = Convert.ToInt32(ConfigurationManager.AppSettings["pauseFor"]) * 1000;
+            _processName = ConfigurationManager.AppSettings["processName"];
+
+            int TwoThreadsForNonVegas = Convert.ToInt32(ConfigurationManager.AppSettings["TwoThreadsForNonVegas"]);
+            switch (TwoThreadsForNonVegas)
+            {
+                //  0 = no just 1
+                //  1 = alternate 1 and 2
+                //  2 = 2 always
+
+                case 0:
+                    _twoNonVegaThreads = false;
+                    break;
+                case 1:
+                    if (_twoNonVegaThreads)
+                        _twoNonVegaThreads = false;
+                    else
+                        _twoNonVegaThreads = true;
+                    break;
+                case 2:
+                    _twoNonVegaThreads = true;
+                    break;
+            }
+               
+
+            if (Convert.ToInt32(ConfigurationManager.AppSettings["TwoThreadsForNonVegas"]) == 0)
+                _twoNonVegaThreads = false;
+
+
+        }
+
+        private void ShutDownWindows()
+        {
+            var psi = new ProcessStartInfo("shutdown", "/r /f /t 0");
+
+            psi.CreateNoWindow = true;
+            psi.UseShellExecute = false;
+            Process.Start(psi);
+        }
+        private void Log(string message, bool error) {
+            
+
+
+            System.IO.StreamWriter sw = new System.IO.StreamWriter("Log.txt",true);
+            sw.WriteLine(DateTime.Now.ToString() + " " + message);          
+            sw.Flush();
+            sw.Close();
+            sw.Dispose();
+            if (error)
+            {
+                errCount++;
+                lblErrorCount.Text = errCount.ToString();
+                timer1.Interval = _pauseFor;
+            }
+         
+        }
+
+
+        /*
+                private void RunMiner()
+                {
+
+                    //   InitVariables();
+
+
+                       var proc = new Process();
+                       proc.StartInfo.FileName = _processName  ;
+                       proc.Start();
+                       proc.Close();
+
+
+                       StartProcess(_processName);
+                       timer1.Interval = _runFor ;
+                       run = true;
+                       Log("Miner ran",false);
+
+                   }
+                    */
+        private void KillProcess(string processNmae)
+        {
+
+            Process[] processes = Process.GetProcessesByName(processNmae);
+            foreach (var process in processes)
+            {
+                process.Kill();
+            }
+        }
+
+
+        private void StartProcess(string processName)
+        {
+            var proc = new Process();
+            proc.StartInfo.FileName = processName;
+           //proc.StartInfo.Verb = "runas";
+            proc.Start();
+            proc.Close();
+        }
+        private void PauseMiner()
+        {
+
+            try
+            {
+               // InitVariables();
+                KillProcess(_processName);
+                timer1.Interval = _pauseFor;
+                run = false;
+                Log("Miner Paused", false);
+
+               
+            }
+
+            catch (Exception ex)
+            {
+                Log(ex.ToString(), true);
+                ShutDownWindows();
+            }
+        }
+
+
+   
+
+
+        private void StartMiner()
+        {
+
+            try
+            {
+                InitVariables();
+                ConfigFile_Builder();
+                StartProcess(_processName);
+                timer1.Interval = _runFor;
+                run = true;
+                Log("Miner ran", false);
+                timer1.Enabled = true;
+
+
+            }
+            catch (Exception ex)
+            {
+                Log(ex.ToString(), true);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartMiner();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PauseMiner();
+        }
+
+        private void ResetGPUs()
+        {
+            StartProcess("ResetGPUsElev");
+            System.Threading.Thread.Sleep(10000);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Process.GetProcessesByName("StartStop").Length > 1)
+                {
+                    Log("Attempting to run this app twice. Exiting this instance now.", true);
+                    Application.Exit();
+                }
+                else
+                {
+
+                    ResetGPUs();
+                    StartMiner();
+                }
+            }
+            catch (Exception ex)
+            {
+                Log(ex.ToString(), true);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (run)
+                {
+                    PauseMiner() ;
+                }
+                else
+                {
+                    StartMiner();
+                }
+            }
+            catch (Exception ex)
+            {
+                Log(ex.ToString(),true);
+                ShutDownWindows();
+
+            }
+        }
+
+        private void txtRunFor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPauseFor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ConfigFile_Builder()
+        {
+
+
+           // bool TwoThreadsForNonVegas = Convert.ToBoolean( AppSetting("TwoThreadsForNonVegas"));
+            ManagementObjectSearcher objvide = new ManagementObjectSearcher("select * from Win32_VideoController");
+            int gpu_thread_num = 0;
+            int index = 0;
+            System.Text.StringBuilder sb = new StringBuilder();
+            int numGpus = 0;
+
+            int numGpusIndexed = objvide.Get().Count - 1;
+            numGpus++;
+
+            sb.AppendLine("\"gpu_threads_conf\" : [");
+           
+
+            foreach (ManagementObject obj in objvide.Get())
+            {
+
+                string thread_conf = string.Empty;
+                if (obj["Name"].ToString().ToLower().Contains("vega"))
+                {
+                    sb.AppendLine(AppSetting("ThreadConfig1Vega56").Replace("ReplaceThisIndex", numGpusIndexed.ToString()));
+                    sb.AppendLine(AppSetting("ThreadConfig2Vega56").Replace("ReplaceThisIndex", numGpusIndexed.ToString()));                 
+                    gpu_thread_num = gpu_thread_num + 2;
+                }
+                else
+                {                
+                    sb.AppendLine(AppSetting("ThreadConfig1NonVega").Replace("ReplaceThisIndex", index.ToString()));
+                    gpu_thread_num++;
+                    if (_twoNonVegaThreads) {                     
+                        sb.AppendLine(AppSetting("ThreadConfig2NonVega").Replace("ReplaceThisIndex", index.ToString()));
+                        gpu_thread_num++;
+                    }
+                    index++;
+                   
+                }               
+
+                    
+
+            }
+
+
+
+            sb.AppendLine("],");
+
+
+            System.IO.StreamWriter sw = new System.IO.StreamWriter("config.txt", false);
+            sw.WriteLine("\"gpu_thread_num\" : " + gpu_thread_num.ToString () + ",");
+            sw.WriteLine(sb.ToString());
+
+            System.IO.StreamReader sr = new System.IO.StreamReader("config_Footer.txt");
+            sw.WriteLine(sr.ReadToEnd());
+
+            sr.Close();
+            sr.Dispose();
+
+            sw.Flush();
+            sw.Close();
+            sw.Dispose();
+
+            /*
+            foreach (ManagementObject obj in objvide.Get())
+            {
+                MessageBox.Show(obj["Name"].ToString());
+
+                Console.Write("Name  -  " + obj["Name"] + "</br>");
+                Console.Write("DeviceID  -  " + obj["DeviceID"] + "</br>");
+                Console.Write("AdapterRAM  -  " + obj["AdapterRAM"] + "</br>");
+                Console.Write("AdapterDACType  -  " + obj["AdapterDACType"] + "</br>");
+                Console.Write("Monochrome  -  " + obj["Monochrome"] + "</br>");
+                Console.Write("InstalledDisplayDrivers  -  " + obj["InstalledDisplayDrivers"] + "</br>");
+                Console.Write("DriverVersion  -  " + obj["DriverVersion"] + "</br>");
+                Console.Write("VideoProcessor  -  " + obj["VideoProcessor"] + "</br>");
+                Console.Write("VideoArchitecture  -  " + obj["VideoArchitecture"] + "</br>");
+                Console.Write("VideoMemoryType  -  " + obj["VideoMemoryType"] + "</br>");
+
+            }
+            */
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            StartProcess(@"list");
+        }
+    }
+
+
+}
